b3f77f42e662ebf8db4480c3c4ff774e6f60695d
MisrudinNamikaze <misrudinz@gmail.com>
2020-03-07 13:17:20 +0700

    map markers multiple

--- src/Screens/Maps.js
+++ src/Screens/Maps.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
 const Maps = () => {
+  const [markers, setMarkers] = useState([
+    {
+      latitude: 37.78825,
+      longitude: -122.4324,
+      title: 'Udin',
+    },
+    {
+      latitude: 37.78625,
+      longitude: -122.4335,
+      title: 'Bapa',
+    },
+    {
+      latitude: 37.78628,
+      longitude: -122.4365,
+      title: 'Ema',
+    },
+    {
+      latitude: 37.78825,
+      longitude: -122.4335,
+      title: 'Adek',
+    },
+  ]);
+
   return (
     <View>
       <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        provider={PROVIDER_GOOGLE}
         style={styles.map}
         region={{
           latitude: 37.78825,
@@ -14,9 +37,17 @@ const Maps = () => {
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}>
-        <Marker
-          coordinate={{latitude: 37.78825, longitude: -122.4324}}
-          title={'hongkiy'}></Marker>
+        {markers.map(marker => {
+          return (
+            <Marker
+              coordinate={{
+                latitude: marker.latitude,
+                longitude: marker.longitude,
+              }}
+              title={marker.title}
+            />
+          );
+        })}
       </MapView>
     </View>
   );
